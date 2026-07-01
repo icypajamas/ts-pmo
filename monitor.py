316a66d3166f9de822b77bbf89d3ee070a0d9b3d
icypajamas <ch.hishamabbas@gmail.com>
2026-07-01 20:41:36 +0500

    Basic skeleton

--- monitor.py
+++ monitor.py
@@ -1,6 +1,59 @@
 import psutil
+import argparse
+import time
+import platform
 
-print("Memory: ", psutil.virtual_memory().percent, "%")
-print("CPU Frequency (Min):", psutil.cpu_freq().min / 1000, "GHz")
-print("CPU Frequency (Max):", psutil.cpu_freq().max / 1000, "GHz")
-print("CPU Frequency:", round(psutil.cpu_freq().current / 1000, 2), "GHz")
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+# parser.add_argument()
+
+args = parser.parse_args()
+
+first = True
+
+
+def cpuPrint():
+    global first
+    if first:
+        print("======= CPU =======")
+        first = False
+
+
+def ramPrint():
+    global first
+    if first:
+        print("======= Memory =======")
+        first = False
+
+
+print("ts-pmo 'This System Python Monitor' - v1.0")
+if args.verbose:
+    print("Hi hi hi")
+    print(psutil.cpu_count())
+else:
+    print("Verbose mode is off. Use -v for more details.")
+    while True:
+
+        # CPU
+        cpu_percent = psutil.cpu_percent()
+        cpu_max = psutil.cpu_freq().max
+        cpu_min = psutil.cpu_freq().min
+        cpuPrint()
+        print(
+            f"\033[2K\rCPU Usage: {cpu_percent}%  |  Max Clock: {cpu_max/1000} GHz  |  Min Clock: {cpu_min/1000} GHz",
+            end="",
+            flush=True,
+        )
+
+        # RAM
+
+        memory_percent = psutil.virtual_memory().percent
+        memory_total = psutil.virtual_memory().total
+        memory_free = psutil.virtual_memory().available
+        ramPrint()
+        print(
+            f"\033[2K\rRAM Usage: {memory_percent}%  |  Total Memory: {memory_total/1000} GB  |  Available Memory: {memory_free/1000} GB",
+            end="",
+            flush=True,
+        )
+        time.sleep(1)
